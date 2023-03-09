54450a54de92265bc5304eb51a7e24addd280c9a
tm-sthm <tm.sthm@gmail.com>
2023-03-09 12:04:36 +0900

    postcss プラグイン追加

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
 module.exports = {
+  parser: require('postcss-comment'),
   plugins: {
     autoprefixer: {},
     cssnano: {},
     "postcss-import": {},
     "postcss-preset-env": {
-        stage:3,
+      stage: 3,
       features: {
         "nesting-rules": true,
+        "media-query-ranges": true,
       },
     },
+    'postcss-sort-media-queries': {},
   },
 };

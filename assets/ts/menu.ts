8d6c4d2dd112ab6f9f619ed5cbb8c4a0183d821b
tm-sthm <tm.sthm@gmail.com>
2023-03-13 16:19:37 +0900

    画面外クリックで閉じる

--- assets/ts/menu.ts
+++ assets/ts/menu.ts
@@ -2,23 +2,31 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.getElementById("open_menu");
   if (!(menuButton instanceof HTMLElement)) return;
 
-  menuButton.addEventListener("click", handler);
+  document.addEventListener("click", handler);
 });
 
 const handler = (e: Event) => {
-  const globalNav = document.getElementById("global_nav");
-  if (!(globalNav instanceof HTMLElement)) return;
-  if (!(e.target instanceof HTMLImageElement)) return;
+  if (!(e.target instanceof HTMLElement)) return;
   
-  if (!e.target.classList.contains("open")) {
-    globalNav.classList.add("open");
-    e.target.classList.add("open");
-    e.target.dataset.orig = e.target.src;
-    e.target.src = e.target.dataset.src ?? "";
-  }
-  else{
+  const globalNav = document.getElementById("global_nav");
+  const menuButton = document.querySelector("#open_menu > img");
+  if (!(globalNav instanceof HTMLElement)) return;
+  if (!(menuButton instanceof HTMLImageElement)) return;
+  
+  if (e.target.closest("#open_menu > img")) {
+    if (!menuButton.classList.contains("open")) {
+      globalNav.classList.add("open");
+      menuButton.classList.add("open");
+      menuButton.dataset.orig = menuButton.src;
+      menuButton.src = menuButton.dataset.src ?? "";
+    } else {
+      globalNav.classList.remove("open");
+      menuButton.classList.remove("open");
+      menuButton.src = menuButton.dataset.orig ?? "";
+    }
+  } else {
     globalNav.classList.remove("open");
-    e.target.classList.remove("open");
-    e.target.src = e.target.dataset.orig ?? "";
+    menuButton.classList.remove("open");
+    menuButton.src = menuButton.dataset.orig ?? "";
   }
 };
